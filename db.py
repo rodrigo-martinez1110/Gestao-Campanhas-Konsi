093584f8e88325ef9488ca7cd8efc427ee1f7ae3
Rodrigo Martinez <rsmartinez1110@gmail.com>
2025-08-06 02:18:53 -0300

    Update db.py

--- db.py
+++ db.py
@@ -16,29 +16,32 @@ supabase = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 
 def inserir_campanha(dados):
     response = supabase.table("campanhas_planejadas").insert(dados).execute()
-    if response.status_code not in [200, 201]:
-        st.error(f"Erro ao inserir campanha: {response.status_code} - {response.json()}")
+    if response.error is not None:
+        st.error(f"Erro ao inserir campanha: {response.error.message}")
+    else:
+        st.success("Campanha inserida com sucesso!")
     
 def listar_campanhas_por_semana(semana_iso):
-    response = supabase \
-        .table("campanhas_planejadas") \
-        .select("*") \
-        .eq("semana_iso", semana_iso) \
-        .order("data_disparo") \
+    response = (
+        supabase.table("campanhas_planejadas")
+        .select("*")
+        .eq("semana_iso", semana_iso)
+        .order("data_disparo")
         .execute()
-
-    if response.status_code == 200:
-        return response.data  # pode usar pd.DataFrame(response.data) se quiser
+    )
+    if response.error is None:
+        return response.data  # que é uma lista de dicionários
     else:
-        st.error("Erro ao listar campanhas: " + str(response.status_code))
+        st.error(f"Erro ao buscar campanhas: {response.error.message}")
         return []
 
-def atualizar_quantidade_disparo(tag_campanha, nova_quantidade):
-    response = supabase \
-        .table("campanhas_planejadas") \
-        .update({"quantidade_disparo": nova_quantidade}) \
-        .eq("tag_campanha", tag_campanha) \
-        .execute()
 
-    if response.status_code not in [200, 204]:
-        st.error("Erro ao atualizar: " + str(response.status_code))
+def atualizar_quantidade_disparo(tag_campanha, nova_quantidade):
+    response = (
+        supabase.table("campanhas_planejadas")
+        .update({"quantidade_disparo": nova_quantidade})
+        .eq("tag_campanha", tag_campanha)
+        .execute()
+    )
+    if response.error is not None:
+        st.error(f"Erro ao atualizar campanha: {response.error.message}")
